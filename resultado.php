45893b9683d93be2f5b03a9af2fe1338d268b74d
Eduardo <schiniguinha@gmail.com>
2021-06-15 09:01:23 -0300

    Seleção de todos os casos do banco de dados

--- resultado.php
+++ resultado.php
@@ -12,10 +12,24 @@
 </html>
 
 <?php
- include_once "conexao.php";
- /** Possiveis solucoes:
-  * ID  -> Mensagem da solucao
-  * 100 -> Provavelmente o problema não está na Internet, mas sim no seu aparelho. Faça...
-  */
+include_once "conexao.php";
+/** Possiveis solucoes:
+* ID  -> Mensagem da solucao
+* 100 -> Provavelmente o problema não está na Internet, mas sim no seu aparelho. Faça...
+*/
 
+//1. Ler dados do BD em um array
+//2. Calcular o nivel de similaridade de cada registro do array com o novo caso
+//3. Calcular qual será a saída
+
+$query = "SELECT * FROM RESPOSTAS;";
+$resultadosBD = mysqli_query($connection, $query);
+$registros = [];
+
+for ($i=0; $i < mysqli_num_rows($resultadosBD); $i++) { 
+  $registros[$i] = mysqli_fetch_row($resultadosBD);
+}
+
+//echo($registros[0][1]);
+//abs()
 ?>
