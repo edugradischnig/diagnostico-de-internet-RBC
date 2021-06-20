1a143950f1869c433c5a148ca7a82a86eeca5162
Eduardo Gradischnig <schiniguinha@gmail.com>
2021-06-20 18:10:04 -0300

    Adição de pesos na fórmula do vizinho mais próximo.

--- feedback.php
+++ feedback.php
@@ -1,5 +1,7 @@
 <?php
 if($_POST['feedback'] == "s"){
+    // 1 - Verificar se ja não existe uma solução igual no banco ()
+    // 2 - Armazenar, ou não, esse caso ()
     echo "<h3>Que bom que funcionou, iremos armazenar seu caso para contribuir com o funcionamento do programa!</h3>";
 }else{
     echo "Codigo para recuperar...";
--- resultado.php
+++ resultado.php
@@ -28,7 +28,11 @@ $valoresDeSimilaridade = array_fill(0, sizeof($registros), 0);
 
 for ($z=0; $z < sizeof($registros); $z++) { 
   for ($i=1; $i < 7; $i++) { 
-    $valoresDeSimilaridade[$z]+= abs($casoUsuario[$i - 1] - intval($registros[$z][$i]));
+    if($i == 2 || $i == 6){
+      $valoresDeSimilaridade[$z]+= abs($casoUsuario[$i - 1] - intval($registros[$z][$i])) * 2; //Para a 2 e 6 pergunta, o peso é dobrado
+    }else{
+      $valoresDeSimilaridade[$z]+= abs($casoUsuario[$i - 1] - intval($registros[$z][$i]));
+    }
   }
 }
 
@@ -45,28 +49,32 @@ $solucao = $registros[$posicaoCasoMaisSimilar][7];
 
 switch ($solucao) {
   case 10:
-    echo "<h3>Possivelmente o problema não é na internet ou nos aparelhos de internet, mas sim no seu dispositivo. 
-    Tente reiniciá-lo, esquecer a rede WiFi novamente e se o problema persistir, entre em contato com a provedora 
-    ou leve-o à uma assistência técnica.</h3>";
+    echo "REINICIAR DISPOSITIVO<br>";
+    //echo "<h3>Possivelmente o problema não é na internet ou nos aparelhos de internet, mas sim no seu dispositivo. 
+    //Tente reiniciá-lo, esquecer a rede WiFi novamente e se o problema persistir, entre em contato com a provedora 
+    //ou leve-o à uma assistência técnica.</h3>";
     break;
   case 11:
-    echo "<h3>Possivelmente o problema está nesse cabo mal conectado ou danificado, então tente reconectá-lo. 
-    Se mesmo assim não funcionar ele está danificado, providencie a troca deste cabo ou entre em contato com 
-    a provedora solicitando uma visita técnica. </h3>";
+    echo "ARRUMAR CABO<br>";
+    //echo "<h3>Possivelmente o problema está nesse cabo mal conectado ou danificado, então tente reconectá-lo. 
+    //Se mesmo assim não funcionar ele está danificado, providencie a troca deste cabo ou entre em contato com 
+    //a provedora solicitando uma visita técnica. </h3>";
     break;
   case 12:
-    echo "<h3>Possivelmente o problema é no link de internet da sua provedora. Aguarde resolução do problema ou entre 
-    em contato com a sua provedora para que possam providenciar a manutenção.</h3>";
+    echo "PROBLEMA COM A PROVEDORA<br>";
+    //echo "<h3>Possivelmente o problema é no link de internet da sua provedora. Aguarde resolução do problema ou entre 
+    //em contato com a sua provedora para que possam providenciar a manutenção.</h3>";
     break;
   case 13:
-    echo "<h3>Possivelmente algum equipamento de internet da sua residência foi danificado ou está queimado. 
-    Entre em contato com a provedora para que prestem assistência técnica para você.</h3>";
+    echo "EQUIPAMENTO DANIFICADO<br>";
+    //echo "<h3>Possivelmente algum equipamento de internet da sua residência foi danificado ou está queimado. 
+    //Entre em contato com a provedora para que prestem assistência técnica para você.</h3>";
     break;
   default:
     echo "<h3>Não conseguimos propor uma solução para o seu problema.</h3>";
     break;
 }
-
+echo "<h3>Ajude a contribuir com nosso programa respondendo se a solução ajudou você a restabelcer a conexão com a internet.</h3>";
 echo "<label>Essa solução funcionou?</label>";
 echo "<form action='feedback.php' method='POST'>";
 echo "<input type='radio' id='feedS' name='feedback' value='s' required>";
