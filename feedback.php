9e9b27566e2cfc0a46d294dceeb02db929b80978
Eduardo Gradischnig <schiniguinha@gmail.com>
2021-06-21 23:11:06 -0300

    Versão estável e funcional.

--- feedback.php
+++ feedback.php
@@ -1,3 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lora&family=Titillium+Web&display=swap" rel="stylesheet">
+
+    <title>Document</title>
+    <style>
+      body{
+        background-color:lightblue;
+      }
+      h3{
+        font-size:30px;
+        text-align: center;
+        font-family: 'Lora', serif;
+      }
+      button {
+        font-family: 'Titillium Web', sans-serif;
+        font-size: 22px;
+        margin-left: 42%;
+        background-color: rgb(211, 211, 211);
+        cursor:pointer;
+        font-weight: bold;
+        border: 1px solid #000
+      }button:hover{
+        background-color: rgb(31, 162, 68);
+      }
+
+    </style>
+</head>
+</style>
+<body>
+</body>
+</html>
 <?php
 include_once "conexao.php";
 
@@ -25,17 +62,17 @@ if($_POST['feedback'] == "s"){
         }
     }
     if(!$casoAtualJaEstaNoBanco){
-        echo "VOU ADICIONAR!!!!!!<br>";
-        echo $_COOKIE['solucao'];
         $query = "INSERT INTO RESPOSTAS (quest1, quest2, quest3, quest4, quest5, quest6, idSolucao) VALUES (".$_COOKIE['casoUsuario'][0]
         .",".$_COOKIE['casoUsuario'][1].",".$_COOKIE['casoUsuario'][2].",".$_COOKIE['casoUsuario'][3].",".$_COOKIE['casoUsuario'][4]
         .",".$_COOKIE['casoUsuario'][5].",".$_COOKIE['solucao'].")";
 
-        //mysqli_query($connection, $query); LINHA DE ACIÇÃO NO BANCO FUNCIONANDO
+        mysqli_query($connection, $query);
     }
+    echo "<br><br>";
+    echo "<a type= 'submit' href='index.html'><button>Voltar a página inicial</button></a>";
    
 }else{
     echo "<h3>Desculpe por não informar a solução correta. Quem sabe você respondeu alguma pergunta de forma equivocada.</h3>";
-    echo "<a href='index.html'><button>Responder novamente</button></a>";
+    echo "<a type= 'submit' href='index.html'><button>Responder novamente</button></a>";
 }
 ?>
--- resultado.php
+++ resultado.php
@@ -4,13 +4,56 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lora&family=Titillium+Web&display=swap" rel="stylesheet">
+
     <title>Solucione o seu problema de internet</title>
+    <style>
+      body{
+        background-color:lightblue;
+        padding: 0 8vw 0 8vw;
+      }
+      h3{
+        font-size:26px;
+        text-align: center;
+        font-family: 'Lora', serif;
+        text-align: justify;
+        color: #2D36DB;
+        padding-top: 5vh;
+      }h4{
+        font-size:22px;
+        text-align: center;
+        font-family: 'Lora', serif;
+      }
+      h1{
+        font-family: 'Titillium Web', sans-serif;
+        text-align: center;
+      }
+      label{
+        font-size:25px;
+        font-family: 'Titillium Web', sans-serif;
+        justify-self:center;
+      }
+      input[type=submit] {
+        font-family: 'Titillium Web', sans-serif;
+        font-size: 22px;
+        margin-left: 47%;
+        margin-top: 30px;
+        background-color: rgb(211, 211, 211);
+        cursor:pointer;
+        font-weight: bold;
+        border: 1px solid #000
+      }input[type=submit]:hover{
+        background-color: rgb(31, 162, 68);
+      }
+      #pergunta{
+        text-align: center;
+      }
+    </style>
 </head>
 <body>
-    
 </body>
 </html>
-
 
 <?php
 include_once "conexao.php";
@@ -50,26 +93,22 @@ setcookie("solucao",$solucao, time() + 600);
 
 switch ($solucao) {
   case 10:
-    echo "REINICIAR DISPOSITIVO<br>";
-    //echo "<h3>Possivelmente o problema não é na internet ou nos aparelhos de internet, mas sim no seu dispositivo. 
-    //Tente reiniciá-lo, esquecer a rede WiFi novamente e se o problema persistir, entre em contato com a provedora 
-    //ou leve-o à uma assistência técnica.</h3>";
+  echo "<h3>Possivelmente o problema não é na internet ou nos aparelhos de internet, mas sim no seu dispositivo. 
+    Tente reiniciá-lo, esquecer a rede WiFi novamente e se o problema persistir, entre em contato com a provedora 
+    ou leve-o à uma assistência técnica.</h3>";
     break;
   case 11:
-    echo "ARRUMAR CABO<br>";
-    //echo "<h3>Possivelmente o problema está nesse cabo mal conectado ou danificado, então tente reconectá-lo. 
-    //Se mesmo assim não funcionar ele está danificado, providencie a troca deste cabo ou entre em contato com 
-    //a provedora solicitando uma visita técnica. </h3>";
+    echo "<h3>Possivelmente o problema está nesse cabo mal conectado ou danificado, então tente reconectá-lo. 
+    Se mesmo assim não funcionar ele está danificado, providencie a troca deste cabo ou entre em contato com 
+    a provedora solicitando uma visita técnica. </h3>";
     break;
   case 12:
-    echo "PROBLEMA COM A PROVEDORA<br>";
-    //echo "<h3>Possivelmente o problema é no link de internet da sua provedora. Aguarde resolução do problema ou entre 
-    //em contato com a sua provedora para que possam providenciar a manutenção.</h3>";
+    echo "<h3>Possivelmente o problema é no link de internet da sua provedora. Aguarde resolução do problema ou entre 
+    em contato com a sua provedora para que possam providenciar a manutenção.</h3>";
     break;
   case 13:
-    echo "EQUIPAMENTO DANIFICADO<br>";
-    //echo "<h3>Possivelmente algum equipamento de internet da sua residência foi danificado ou está queimado. 
-    //Entre em contato com a provedora para que prestem assistência técnica para você.</h3>";
+    echo "<h3>Possivelmente algum equipamento de internet da sua residência foi danificado ou está queimado. 
+    Entre em contato com a provedora para que prestem assistência técnica para você.</h3>";
     break;
   default:
     echo "<h3>Não conseguimos propor uma solução para o seu problema.</h3>";
@@ -77,13 +116,16 @@ switch ($solucao) {
 }
 setcookie("casoUsuario",$_POST["q1"].$_POST["q2"].$_POST["q3"].$_POST["q4"].$_POST["q5"].$_POST["q6"], time() + 600);
 
-echo "<h3>Ajude a contribuir com nosso programa respondendo se a solução ajudou você a restabelcer a conexão com a internet.</h3>";
-echo "<label>Essa solução funcionou?</label>";
+echo "<br><br><br>";
+echo "<div id='pergunta'>";
+echo "<h4>Ajude a contribuir com nosso programa respondendo se a solução acima ajudou você a restabelcer a conexão com a internet.</h3>";
+echo "<label class='pergunta'>Essa solução funcionou?</label>";
 echo "<form action='feedback.php' method='POST'>";
 echo "<input type='radio' id='feedS' name='feedback' value='s' required>";
 echo "<label for='feedS'>Sim</label><br>";
 echo "<input type='radio' id='feedN' name='feedback' value='n' required>";
 echo "<label for='feedN'>Não</label><br>";
+echo "</div>";
 echo "<input type='submit' value='Enviar'>";
 echo "</form>";
 ?>
